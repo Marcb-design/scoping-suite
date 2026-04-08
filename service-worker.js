ec785813bcfb4664377e1000fda958ced9ed8c03
Marcb-design <marc@varlon.co.uk>
2026-04-08 07:51:23 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+﻿const CACHE_NAME = "rail-scoping-suite-lite-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./access/index.html",
+  "./track/index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./img/rail-hero.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => networkResponse)
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
